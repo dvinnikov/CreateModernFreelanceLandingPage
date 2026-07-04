79f691b3b7893317769fd661772e9576fdd7e1f4
dvinnikov <samui2006@ukr.net>
2026-07-04 08:27:28 +0300

    Route admin stats page from app entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
+import AdminStats from "./app/AdminStats";
 import { initSiteAnalytics } from "./lib/siteAnalytics";
 import "./styles/index.css";
 
-initSiteAnalytics();
+const isAdminStats = window.location.pathname === "/admin/stats";
 
-createRoot(document.getElementById("root")!).render(<App />);
+if (!isAdminStats) {
+  initSiteAnalytics();
+}
+
+createRoot(document.getElementById("root")!).render(isAdminStats ? <AdminStats /> : <App />);
